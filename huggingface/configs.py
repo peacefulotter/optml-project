f4ef062d79023f569ffa7651c8f41338a6a842c5
PeacefulOtter <zankro67@gmail.com>
2023-06-09 12:26:50 +0200

    Added config options demo

--- huggingface/configs.py
+++ huggingface/configs.py
@@ -5,6 +5,7 @@ TRAINING_CONFIGS = {
     'bert-wikitext': {
         'tokenizer_name': 'bert-base-cased',
         'dataset_path': 'wikitext', # 'openwebtext'
-        'dataset_name': 'wikitext-103-raw-v1'
+        'dataset_name': 'wikitext-103-raw-v1',
+        # 'learning_rates': np.linspace(1e-4, 1e-5)
     }
 }
